a65810ce3ff9db78685756901f3c81d507fdcb69
Amit Chauhan <amichauh@microsoft.com>
2024-05-04 21:19:11 +0530

    ArrayBasic1

---
+++ Array/ArrayBasic/ArrayBasic/FindSecondLargestElementInArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+/// <summary>
+/// Given an array, find the second smallest and second largest element in the array. Print ‘-1’ in the event that either of them doesn’t exist.
+/// </summary>
+
+public class FindSecondLargestElementInArray
+{
+    //Optimal ->  T -> O(N)
+    public static void Run(int[]  arr)
+    {
+        int n = arr.Length;
+        int largest = Int32.MinValue;
+        int slargest = Int32.MinValue;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > largest)
+            {
+                slargest = largest;
+                largest = arr[i];
+            }else if (arr[i] < largest && arr[i] > slargest)
+            {
+                slargest = arr[i];
+            }
+        }
+
+        Console.WriteLine("Printing input array: ");
+        foreach (var i in arr)
+        {
+            Console.Write($"{i} ");
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Second Largest: {slargest}");
+
+    }
+
+}
---
+++ Array/ArrayBasic/ArrayBasic/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ArrayBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindSecondLargestElementInArray.Run([2,3,1,45,2,12,32,36,1,4]);
+        }
+    }
+}
