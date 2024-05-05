1b0f5e6723ea86059838553c59f663d263f659f4
Amit Chauhan <amichauh@microsoft.com>
2024-05-05 12:35:32 +0530

    Subarray with sum K cases

---
+++ Array/ArrayBasic/ArrayBasic/CountSubarraySumEqualsK.cs
@@ -0,0 +1,74 @@
+﻿namespace ArrayBasic
+{
+    class LongestSubArrayWithKSum
+    {
+        /// <summary>
+        /// Given an array and a sum k, we need to print the length of the longest subarray that sums to k.
+        ///</summary>
+
+        /// Better -> using Hashing and storing prefix sum.
+        /// Optimal for both positive and negative elements
+        public int Run(int[] arr, int k)
+        {
+            int n = arr.Length;
+
+            Dictionary<int, int> mp = new();
+            int sum = 0;
+            int ans = 0;
+            
+            for (int i = 0; i < n; i++)
+            {
+                sum += arr[i];
+
+                if (sum == k)
+                    ans = int.Max(ans, i + 1);
+                mp.GetValueOrDefault(sum);
+                int rem = sum - k;
+
+                if(mp.ContainsKey(rem))
+                {
+                    int remInd = mp[rem];
+                    ans = int.Max(ans,i-remInd);
+                }
+
+                if (!mp.ContainsKey(sum))
+                {
+                    mp.Add(sum, i);
+                }
+                
+            }
+
+
+            return ans;
+        }
+        //optimal only for Postive.
+        public int Run2(int[] arr, int k)
+        {
+            int l = 0, r = 0;
+            int sum = arr[0];
+            int n= arr.Length;
+
+            int ans = 0;
+            while (r < n) { 
+            
+                if(sum>k && l<=r)
+                {
+                    sum -= arr[l];
+                    l++;
+                }
+
+                if (sum == k)
+                {
+                    ans = int.Max(ans, l);
+                }
+
+                r++;
+                if(r<n)
+                    sum += arr[r];
+            
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ Array/ArrayBasic/ArrayBasic/LongestSubArrayWithKSum.cs
@@ -0,0 +1,30 @@
+﻿namespace ArrayBasic
+{
+    class CountSubarraySumEqualsK
+    {
+        /// <summary>
+        /// Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+        ///</summary>
+
+        /// Optimal for both positive and negative elements
+        public int Run(int[] arr, int k)
+        {
+            int n = arr.Length;
+            Dictionary<int, int> mp = new();
+            int sum = 0;
+            int ans = 0;
+            mp[0] = 1;
+            for (int i = 0; i < n; i++)
+            {
+                sum += arr[i];
+                if (mp.ContainsKey(sum - k))
+                    ans += mp[sum - k];
+
+                mp[sum] = mp.GetValueOrDefault(sum, 0) + 1;
+
+            }
+
+            return ans;
+        }
+    }
+}
